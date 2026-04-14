7059bc954a3a71c8350dbb4621d139ee50d8c82e
PatoMan705 <78129062+PatoMan705@users.noreply.github.com>
2026-04-14 16:42:31 -0300

    Completamos #24

---
+++ EngineGDI/Src/Defeat.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+
+namespace EngineGDI.Src
+{
+    public class Defeat : Node
+    {
+        public Image defeatScreen = Image.FromFile("Assets/Imgs/defeatPlaceHolder.png");
+
+        public bool toggle = false;
+
+        public void EnableDefeat()
+        {
+            toggle = true;
+        }
+
+        public void DisableDefeat()
+        {
+            toggle = false;
+        }
+
+        public override void Draw()
+        {
+            if (toggle)
+                Engine.Draw(texture: defeatScreen, x: 0, y: 0);
+        }
+    }
+}
