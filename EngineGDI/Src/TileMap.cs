a5bfda4b87b2c389a3a0bad3daab2493e4dd64f6
Luciano Graziani <lgraziani2712@gmail.com>
2026-07-07 19:56:52 -0300

    add missing image

--- EngineGDI/Src/TileMap.cs
+++ EngineGDI/Src/TileMap.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Drawing;
+using System.IO;
 using PlantUmlClassDiagramGenerator.Attributes;
 
 namespace EngineGDI.Src
@@ -8,21 +10,52 @@ namespace EngineGDI.Src
         [PlantUmlIgnoreAssociation]
         public static int Size { get; } = 32;
 
+        private static Bitmap LoadBitmapSafely(string path)
+        {
+            byte[] imageBytes = File.ReadAllBytes(path);
+
+            using MemoryStream ms = new(imageBytes);
+
+            return new Bitmap(ms);
+        }
+
+        private static Bitmap CropImage(Bitmap source, Rectangle cropRect)
+        {
+            Bitmap result = new(cropRect.Width, cropRect.Height, source.PixelFormat);
+            result.SetResolution(source.HorizontalResolution, source.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(
+                    source,
+                    new Rectangle(0, 0, cropRect.Width, cropRect.Height),
+                    cropRect,
+                    GraphicsUnit.Pixel
+                );
+            }
+            return result;
+        }
+
         public static Bitmap LoadSprite(string path, int column, int row)
         {
-            using Bitmap sourceImage = new(filename: path);
-            Rectangle cropRect = new(x: column * Size, y: row * Size, width: Size, height: Size);
+            string fullPath = Path.IsPathRooted(path)
+                ? path
+                : Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
 
-            return sourceImage.Clone(rect: cropRect, format: sourceImage.PixelFormat);
+            using Bitmap sourceImage = LoadBitmapSafely(fullPath);
+            Rectangle cropRect = new(column * Size, row * Size, Size, Size);
+            return CropImage(sourceImage, cropRect);
         }
 
         public static Bitmap LoadSprite(string path, int x, int y, Size size)
         {
-            using Bitmap sourceImage = new(filename: path);
+            string fullPath = Path.IsPathRooted(path)
+                ? path
+                : Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
 
-            Rectangle cropRect = new(x: x, y: y, width: size.Width, height: size.Height);
-
-            return sourceImage.Clone(rect: cropRect, format: sourceImage.PixelFormat);
+            using Bitmap sourceImage = LoadBitmapSafely(fullPath);
+            Rectangle cropRect = new(x, y, size.Width, size.Height);
+            return CropImage(sourceImage, cropRect);
         }
     }
 }
